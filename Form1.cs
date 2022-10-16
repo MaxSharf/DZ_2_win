30ad6e6375b1b88b57a1f4cc70b90e9cfc73005d
Sara_xy66@student.itstep.org <Sara_xy66@student.itstep.org>
2022-10-16 11:13:04 +0300

    Завдання 1 виконано

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,58 @@ namespace DZ_2_win
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Max");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sharf");
+
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Sergiovich");
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("18.03.2004");
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Leave in Zhmerinka");
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Men");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("18");
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Без баб");
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Mision comlite");
+
+        }
     }
 }
